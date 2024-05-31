2575cb3c467c7af0aba370bfc81ffee581a56ba2
Sahildhargave <sahildhargave5253.com>
2024-05-31 17:43:19 +0530

    wroking on thw auth and user resolver and service

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserResolver } from './user.resolver';
+import {PrismaService } from 'src/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [UserService, UserResolver]
+  providers: [UserService, UserResolver,PrismaService, JwtService],
 })
-export class UserModule {}
+export class UserModule {
+
+	
+}
+
